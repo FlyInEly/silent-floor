412788eb1f9502587ec6fe4f4f38d93251335f99
FlyInEly <jw.32640@gmail.com>
2024-06-15 20:19:40 -0700

    1.1.1 - Fix Messenger overloading to use chaining

--- src/client/java/net/flyinely/silentfloor/system/Messenger.java
+++ src/client/java/net/flyinely/silentfloor/system/Messenger.java
@@ -18,6 +18,9 @@ public class Messenger {
         MC.player.networkHandler.sendChatMessage(message.getString());
     }
 
+    public void sendCommand(Text command) {
+        sendCommand(command.getString());
+    }
     public void sendCommand(String command) {
         if (MC.player == null) {
             // ?Add logger error
@@ -25,13 +28,7 @@ public class Messenger {
         }
         MC.player.networkHandler.sendChatCommand(command);
     }
-    public void sendCommand(Text command) {
-        if (MC.player == null) {
-            // ?Add logger error
-            return;
-        }
-        MC.player.networkHandler.sendChatCommand(command.getString());
-    }
+
 
     public void display(String message) {
         display(Text.of(message), false);
@@ -40,11 +37,7 @@ public class Messenger {
         display(message, false);
     }
     public void display(String message, boolean useActionBar) {
-        if (MC.player == null) {
-            // ?Add logger error
-            return;
-        }
-        MC.player.sendMessage(Text.of(message), useActionBar);
+        display(Text.of(message), useActionBar);
     }
     public void display(Text message, boolean useActionBar) {
         if (MC.player == null) {
@@ -54,11 +47,11 @@ public class Messenger {
         MC.player.sendMessage(message, useActionBar);
     }
 
-    public void prepareChat(String message) {
-        MC.setScreen(new ChatScreen(message));
-    }
     public void prepareChat(Text message) {
         prepareChat(message.getString());
+    }
+    public void prepareChat(String message) {
+        MC.setScreen(new ChatScreen(message));
     }
 
 }
